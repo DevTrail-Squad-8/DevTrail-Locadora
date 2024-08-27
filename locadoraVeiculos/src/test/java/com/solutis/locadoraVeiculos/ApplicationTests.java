be075536726c25b2c9ed451e13b5d968799b5f2e
GustavoSilvalgs <luisgust06@gmail.com>
2024-08-26 22:39:25 -0300

    feat: inicializar o projeto Locadora de Veículos com dependências Spring Boot
    - Configurado o projeto Maven com dependências Spring Boot: Data JPA, Web, H2 Database, Lombok e Teste.
    - Criado o Application.java para iniciar a aplicação.

---
+++ locadoraVeiculos/src/test/java/com/solutis/locadoraVeiculos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.locadoraVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
