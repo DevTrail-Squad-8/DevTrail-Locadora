4549da3cc7595d47caa7584707e6c57befc329bc
Thiago Sales <thiagosales46@gmail.com>
2024-09-02 02:18:18 -0300

    Refactor(): remoção do carrinhho e seus respectivas classes

--- locadoraVeiculos/src/main/java/com/solutis/locadoraVeiculos/repository/CarrinhoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.solutis.locadoraVeiculos.repository;
-
-import com.solutis.locadoraVeiculos.model.Carrinho;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrinhoRepository extends JpaRepository<Carrinho, Long> {
-}
-
